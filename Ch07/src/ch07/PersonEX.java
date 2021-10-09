511daa54f99c33b681c18780df2d18ba1f4f6678
XCodeBlocks <xcodeblocks@gmail.com>
2021-10-09 13:21:20 +0900

    2021/10/08-2.5
    (classes name change)

--- Ch07/src/ch07/PersonEX.java
+++ Ch07/src/ch07/PersonEX.java
@@ -1,27 +1,27 @@
 //[promotion, downcasting(강제 형변환)]:
 package ch07;
 
-class Person {
+class Person_ {
 	String name;
 	String id;
 	
-	public Person(String name) {
+	public Person_(String name) {
 		this.name = name;
 	}
 }
-class Student extends Person {
+class Student_ extends Person_ {
 	String grade;
 	String department;
-	public Student(String name) {super(name);}
+	public Student_(String name) {super(name);}
 }
 
 //(실행부)
 public class PersonEX {
 	public static void main(String[] args) {
-		Person p;
-		Student s = new Student("홍길동");
+		Person_ p;
+		Student_ s = new Student_("홍길동");
 		p = s;	//promotion(upcasting)
-		Person p_ = new Student("이름");	//(promotion을 한 줄로)
+		Person_ p_ = new Student_("이름");	//(promotion을 한 줄로)
 		
 		//원래 부모클래스에 존재하는 필드는 접근가능
 		System.out.println(p.name);
@@ -30,7 +30,7 @@ public class PersonEX {
 		//p.department="Com";
 		
 		//강제 형변환(downcasting)
-		s = (Student) p;	//원래 자식타입으로 변환
+		s = (Student_) p;	//원래 자식타입으로 변환
 		
 		//가려졌던 자식영역의 필드와 메소드에 접근 가능
 		System.out.println(s.name);
